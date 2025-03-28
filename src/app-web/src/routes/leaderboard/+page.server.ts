c9391e7ed11292e9ccf36611173ac738d31c6f59
Dennis Skoko <dennis.skoko@hotmail.com>
2025-03-28 23:24:33 +0000

    Make leaderboard prettier

--- src/app-web/src/routes/leaderboard/+page.server.ts
+++ src/app-web/src/routes/leaderboard/+page.server.ts
@@ -3,5 +3,13 @@ import * as leaderboard from '$lib/server/leaderboard'
 
 export const load: PageServerLoad = async () => {
   const result = await leaderboard.list()
-  return { leaderboard: result.filter(entry => entry.name !== null) }
+
+  return {
+    leaderboard: result
+      .filter(entry => entry.name !== null)
+      // Sort on highest score first
+      .sort((a, b) => b.score - a.score)
+      .map(entry => ({ name: entry.name, score: entry.score }))
+      .slice(0, 10)
+  }
 }
--- src/app-web/src/routes/leaderboard/+page.svelte
+++ src/app-web/src/routes/leaderboard/+page.svelte
@@ -8,6 +8,45 @@
 
 <ol>
   {#each data.leaderboard as entry}
-    <li>{entry.name} - {entry.score} points - {entry.search}</li>
+    <li>
+      <span>{entry.name}</span>
+      <span>{entry.score}p</span>
+    </li>
   {/each}
 </ol>
+
+<style>
+  h1 {
+    margin: 2em 0;
+    text-align: center;
+  }
+
+  ol {
+    list-style: none;
+    margin: 0 auto;
+    max-width: 40em;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+  }
+
+  li {
+    border: thin solid #08424d;
+    padding: .5em;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  li:nth-child(1) {
+    font-size: 1.4em;
+  }
+
+  li:nth-child(2) {
+    font-size: 1.3em;
+  }
+
+  li:nth-child(3) {
+    font-size: 1.2em;
+  }
+</style>
